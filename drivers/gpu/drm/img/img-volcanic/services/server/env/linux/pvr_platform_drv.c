613d39be5e805ba7da11e763df87b6114bbec62d
Jaychand Agrawal <jaychand.agrawal@einfochips.com>
2025-07-25 15:06:56 +0530

    Fix build errors for iGPU
    1. Added patch 23512ac3951d0803ed83e6b703836196a1bd4976.
    2. Fix build error for build successful.
    
    Signed-off-by: Jaychand Agrawal <jaychand.agrawal@einfochips.com>

--- drivers/gpu/drm/img/img-volcanic/services/server/env/linux/pvr_platform_drv.c
+++ drivers/gpu/drm/img/img-volcanic/services/server/env/linux/pvr_platform_drv.c
@@ -211,7 +211,7 @@ err_drm_dev_put:
 	return	ret;
 }
 
-static int pvr_remove(struct platform_device *pdev)
+static void pvr_remove(struct platform_device *pdev)
 {
 	struct drm_device *ddev = platform_get_drvdata(pdev);
 
@@ -226,7 +226,6 @@ static int pvr_remove(struct platform_device *pdev)
 	pvr_drm_unload(ddev);
 
 	drm_dev_put(ddev);
-	return 0;
 }
 
 static void pvr_shutdown(struct platform_device *pdev)
